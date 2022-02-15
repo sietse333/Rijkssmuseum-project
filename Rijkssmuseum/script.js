efc2744fac20eb2ed74a2df862f107a3660476c5
sietse333 <sietse333@gmail.com>
2022-02-15 15:01:17 +0100

    mooie animaties esketit

--- Rijkssmuseum/script.js
+++ Rijkssmuseum/script.js
@@ -1,15 +1,57 @@
-const testSection = document.querySelector('#test');
+const Sectionvak = document.querySelector('#test');
+const emptytext = document.querySelector('#empty');
+const fotoFoto = document.querySelector('.fotofoto');
+const fotoText = document.querySelector('.fototext');
 
 
-const getAndRenderData =  () => {
-    const getURL = 'https://www.rijksmuseum.nl/api/nl/collection?key=2mU4mudb&involvedMaker=Rembrandt+van+Rijn&ps=25'  
+function displayLoading() {
+    Sectionvak.classList.add("skeleton");
+    setTimeout(() => {
+        Sectionvak.classList.remove("skeleton");
+        emptytext.textContent = "Failed to load paintings please try again!";
+    }, 4000);
+}
+
+function hideLoading() {
+    Sectionvak.classList.remove("skeleton");
+}
+
+displayLoading()
+
+const getAndRenderData = () => {
+    const getURL = 'https://www.rijksmuseum.nl/api/nl/collection?key=2mU4mudb&involvedMaker=Rembrandt+van+Rijn&ps=25'
     fetch(getURL).then(response => response.json())
-      .then(response => {
-        console.log(response.artObjects)
-        response.artObjects.forEach(kunst => {
-          testSection.insertAdjacentHTML( 'afterbegin', `<article><h2>${kunst.title}</h2> <img src="${kunst.webImage.url}"></article>`)
-        })
-      }).catch(error => document.body.insertAdjacentHTML('beforebegin', error))
-  }
-  getAndRenderData()
+        .then(response => {
+            hideLoading()
+            emptytext.textContent = "";
+            response.artObjects.forEach(kunst => {
+                Sectionvak.insertAdjacentHTML('afterbegin', `<article class="hallo"><h2>${kunst.title}</h2> <img src="${kunst.webImage.url}"></article>`)
+            })
+            generateObserver();
+        }).catch(error => document.body.insertAdjacentHTML('beforebegin', error))
+}
 
+getAndRenderData()
+
+
+
+function generateObserver(){
+//Intersection Observer experiment
+const observer = new IntersectionObserver(showandhideArticles); // er wordt intersection object aangemaakt
+const elements = document.querySelectorAll('.hallo');
+elements.forEach(element => {
+  observer.observe(element) 
+
+});
+}
+
+function showandhideArticles(entries, observer) { 
+  entries.forEach(entry => {
+    const targetClass = entry.target.classList;
+    if(entry.isIntersecting) {
+      targetClass.add('observed')
+    } else {
+      targetClass.remove('observed')
+    }
+  });
+};
