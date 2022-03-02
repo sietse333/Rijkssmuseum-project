2cc5be51e56784c4a0150dd8053cb062c05408b7
sietse333 <sietse333@gmail.com>
2022-03-02 10:12:42 +0100

    styling and cleaning

--- Rijkssmuseum/script.js
+++ Rijkssmuseum/script.js
@@ -1,11 +1,6 @@
 import { url} from './modules/vars.js'
 import { getData } from './modules/data.js'
-
-import { renderHTML } from './modules/render.js'
-import { showandhideArticles } from './modules/showandhide.js'
-import { generateObserver } from './modules/observer.js'
 import { searchBalk, search } from './modules/searchBalk.js'
-import { displayLoading, hideLoading } from './modules/loading.js'
 
 
 getData(url)
